b8f998bc9e5c203c81b0a261175f117f253e58b2
seungbin0508 <seungbin0508@gmail.com>
2021-07-26 14:42:45 +0900

    Add express app
    Set port using .env

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+
+dotenv.config()
+
+const app = new express()
+
+app.set('port', process.env.PORT)
+
+app.listen(app.get('port') || 3000, () => {
+	console.log(`Server listening on port ${app.get('port')}`)
+})
