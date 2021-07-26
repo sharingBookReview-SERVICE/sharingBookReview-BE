6af32e07c1dd9811060cb76cdcbcc1516847ef0f
seungbin0508 <seungbin0508@gmail.com>
2021-07-26 16:11:34 +0900

    Ensure throwing error when requesting invalid router

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.set('port', process.env.PORT)
 
 app.use(router)
 
+app.use((req, res, next) => {
+	const error = new Error(`${req.method} ${req.url} router doesn't exist`)
+	error.status = 404
+	next(error)
+})
+
 app.listen(app.get('port') || 3000, () => {
 	console.log(`Server listening on port ${app.get('port')}`)
 })
