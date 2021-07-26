33ff508afdcbe351225f23f17bc03da4460da646
seungbin0508 <seungbin0508@gmail.com>
2021-07-26 18:29:01 +0900

    Add router's CRUD methods

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,4 +2,24 @@ import express from 'express'
 
 const router = express.Router()
 
+router.post('/:reviewId', (req, res) => {
+
+})
+
+router.get('/:reviewId', (req, res) => {
+
+})
+
+router.put('/:reviewId', (req, res) => {
+
+})
+
+router.delete('/:reviewId', (req, res) => {
+
+})
+
+router.get('/', (req, res) => {
+
+})
+
 export default router
