4529bd8f5d547be853ae8fd24d365df63c6cfd27
seungbin0508 <seungbin0508@gmail.com>
2021-07-26 16:35:41 +0900

    Add sub-routers in /routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
 import express from 'express'
+import usersRouter from './usersRouter.js'
+import booksRouter from './booksRouter.js'
+import reviewsRouter from './reviewsRouter.js'
+import commentsRouter from './commentsRouter.js'
 
 const router = express.Router()
+
+router.use('/api/users', usersRouter)
+router.use('/api/commentsRouter', commentsRouter) // temp path
+router.use('/api/reviewsRouter', reviewsRouter) // temp path
+router.use('/api/booksRouter', booksRouter)
+
+// Swagger
 
 export default router
