54e67f8cb3816f5a1d07729eb7fc57a492f26302
seungbin0508 <seungbin0508@gmail.com>
2021-07-26 15:45:01 +0900

    Add router middleware

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import router from './routes/index.js'
 
 dotenv.config()
 
 const app = new express()
 
 app.set('port', process.env.PORT)
+
+app.use(router)
 
 app.listen(app.get('port') || 3000, () => {
 	console.log(`Server listening on port ${app.get('port')}`)
