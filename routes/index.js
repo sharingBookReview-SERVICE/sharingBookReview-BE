bc93e8b87b7264a3c0e7af66eda4bc0b91df451f
seungbin0508 <seungbin0508@gmail.com>
2021-07-27 09:39:47 +0900

    Commented out incomplete routes

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 import express from 'express'
-import usersRouter from './usersRouter.js'
-import booksRouter from './booksRouter.js'
-import reviewsRouter from './reviewsRouter.js'
-import commentsRouter from './commentsRouter.js'
+// import usersRouter from './usersRouter.js'
+// import booksRouter from './booksRouter.js'
+import reviewsRouter from './reviews.js'
+// import commentsRouter from './commentsRouter.js'
 
 const router = express.Router()
 
-router.use('/api/users', usersRouter)
-router.use('/api/commentsRouter', commentsRouter) // temp path
+// router.use('/api/users', usersRouter)
+// router.use('/api/commentsRouter', commentsRouter) // temp path
 router.use('/api/reviewsRouter', reviewsRouter) // temp path
-router.use('/api/booksRouter', booksRouter)
+// router.use('/api/booksRouter', booksRouter)
 
 // Swagger
 
