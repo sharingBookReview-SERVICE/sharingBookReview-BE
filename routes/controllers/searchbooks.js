22ac83dc5db0f3199f76bd69c7061ccf59745006
ohbinkwon <kobbb2100@gmail.com>
2021-07-28 22:22:24 +0900

    init controllers/searchbooks.js

---
+++ routes/controllers/searchbooks.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+exports.searchbooks = (req,res) => {
+    const client_id = process.env.BOOK_API_CLIENT_ID
+    const client_secret = process.env.BOOK_API_CLIENT_SECRET
+}
+
+
