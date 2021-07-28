da6a8313eae990b21c97627eea737ada25b8ca88
YJ <lyjoo7413@naver.com>
2021-07-28 14:39:20 +0900

    add_ booksRouter in app.js
    add_ booksRouter in app.js

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,13 @@ import usersRouter from './users.js'
 // import booksRouter from './booksRouter.js'
 import reviewsRouter from './reviews.js'
 import commentsRouter from './comments.js'
+import booksRouter from './books.js'
 
 const router = express.Router()
 
 router.use('/api/users', usersRouter)
 router.use('/api/books/:bookId/reviews/:reviewId/comments', commentsRouter) // temp path
 router.use('/api/books/:bookId/reviews', reviewsRouter) // temp path
-// router.use('/api/booksRouter', booksRouter)
-
-// Swagger
+router.use('/api/booksRouter', booksRouter)
 
 export default router
