d050f3f7d9d0df9cf6c81a7b1a9b5094c93e7e82
seungbin0508 <seungbin0508@gmail.com>
2021-07-28 22:05:57 +0900

    Add connection to mongodb

--- models/index.js
+++ models/index.js
@@ -6,4 +6,24 @@ const connect = () => {
 	}
 }
 
+mongoose.connect(
+	'mongodb://localhost:27017/admin',
+	{
+		dbName: 'bns',
+		user: 'sbk',
+		pass: process.env.DB_PASSWORD,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	(err) => {
+		if (err) console.error('몽고디비 연결 오류', err)
+		else console.log('몽고디비 연결 성공')
+	}
+)
+
+mongoose.connection.on('error', (err) => {
+	console.error('몽고디비 연결 에러', err)
+	connect()
+})
+
 connect()
