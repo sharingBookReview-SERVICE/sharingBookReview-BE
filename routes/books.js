562636aed5c68a1a24a0c9a60d9bccc0cc03ad6a
YJ <lyjoo7413@naver.com>
2021-07-28 13:48:24 +0900

    modify_books.js
    modify_books.js

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,7 @@ import express from 'express'
 const router = new express.Router({ mergeParams: true })
 
 const sampleBooks = [
-	(book1 = {
+	{
 		title: '지구인만큼 지구를 사랑할 순 없어',
 		link: 'http://www.kyobobook.co.kr/product/detailViewKor.laf?mallGb=KOR&ejkGb=KOR&barcode=9791191583793&orderClick=s1a',
 		image: 'http://image.kyobobook.co.kr/images/book/large/793/l9791191583793.jpg',
@@ -15,7 +15,7 @@ const sampleBooks = [
 		description:
 			'지구 구석구석 모두의 반짝이는 안녕을 바라며 빛과 사랑의 방향으로 한걸음 나아가는 여행',
 		pubdate: '2021년 06월 10일',
-	}),
+	},
 ]
 
 const sampleBestseller = [
@@ -36,19 +36,15 @@ const sampleBestseller = [
 
 // 책 목록
 router.get('/books', (req, res) => {
-    console.log(sampleBooks)
-    return res.json(sampleBooks)
-    )
-})
-
-// 개별 책 선택
-router.get('/:bookId', (req, res) => {
-	return res.json(sampleBooks.book1)
+	return res.json(sampleBooks)
 })
 
 // 베스트 샐러
 router.get('/bestseller', (req, res) => {
 	return res.json(sampleBestseller)
 })
-
+// 개별 책 선택
+router.get('/:bookId', (req, res) => {
+	return res.json(sampleBooks.book1)
+})
 export default router
