4b243dd0ad2f4a32a5092a94ef26c8e37d1479ee
seungbin0508 <seungbin0508@gmail.com>
2021-07-28 10:45:31 +0900

    Add post, patch and delete methods
    Get method doesn't exist because it is attached to a review

--- routes/comments.js
+++ routes/comments.js
@@ -2,4 +2,16 @@ import express from 'express'
 
 const router = new express.Router({ mergeParams: true })
 
+router.post('/', (req, res) => {
+	return res.sendStatus(201)
+})
+
+router.patch('/:commentId', (req, res) => {
+	return res.sendStatus(200)
+})
+
+router.delete('/:commentId', (req, res) => {
+	return res.sendStatus(200)
+})
+
 export default router
