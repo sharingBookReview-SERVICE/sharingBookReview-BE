3998254767c9542661a11ab2096f63c40e49264c
seungbin0508 <seungbin0508@gmail.com>
2021-07-29 09:57:54 +0900

    Add reviews subdocument

--- models/book.js
+++ models/book.js
@@ -13,6 +13,11 @@ const bookSchema = new mongoose.Schema({
 	discount: Number,
 	description: String,
 	pubdate: Date,
+	reviews: {
+		type: [mongoose.Schema.Types.ObjectId],
+		default: [],
+		ref: 'Review'
+	}
 })
 
 export default mongoose.model('Book', bookSchema)
