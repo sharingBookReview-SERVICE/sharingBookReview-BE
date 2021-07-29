16fa7b1f242d0e3b755bca6f3723b5de54098d17
seungbin0508 <seungbin0508@gmail.com>
2021-07-29 16:51:49 +0900

    Add jsdoc on searchBooks function

--- routes/controllers/searchbooks.js
+++ routes/controllers/searchbooks.js
@@ -1,6 +1,14 @@
 import axios from 'axios'
 import { parseString } from 'xml2js'
 
+/**
+ * Search book information on naver.
+ * @param {string} target - Search target must be either d_titl or d_auth
+ * @param {string} query - Search query
+ * @param {string} client_id - Naver dev api account id
+ * @param {string} client_secret - Naver dev api account secret key
+ * @returns {Promise<Object[]>}
+ */
 const searchBooks = async (target, query, client_id, client_secret) => {
 	const api_url =
 		`https://openapi.naver.com/v1/search/book_adv.xml?${target}=` +
