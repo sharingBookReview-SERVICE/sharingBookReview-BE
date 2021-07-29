898bfb78d34d333385c59461d2d2a2d0692b6ae4
ohbinkwon <kobbb2100@gmail.com>
2021-07-29 13:03:02 +0900

    delete console.log and result message

--- routes/controllers/searchbooks.js
+++ routes/controllers/searchbooks.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const searchBooks = async (target,query,client_id, client_secret) => {
-    console.log(target, query,client_id,client_secret)
     const api_url = "https://openapi.naver.com/v1/search/book.json?query=" + query
         let result = await axios({
             method: 'get',
@@ -12,11 +11,7 @@ const searchBooks = async (target,query,client_id, client_secret) => {
                 "X-Naver-Client-Secret": client_secret
             }
         })
-        
         const resultData = result.data
-        const resultMessage = result.data.message
-
-        console.log(resultMessage)
         return resultData
 
 }
