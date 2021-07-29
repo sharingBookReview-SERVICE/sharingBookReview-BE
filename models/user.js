a9985edb5da39a7972b61b5805367046be1c5666
seungbin0508 <seungbin0508@gmail.com>
2021-07-29 10:56:38 +0900

    Add userSchema

--- models/user.js
+++ models/user.js
@@ -1,7 +1,18 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-
+	nickname: {
+		type: String,
+		required: true,
+	},
+	providerKey: {
+		type: String,
+		required: true,
+	},
+	provider: {
+		type: String,
+		required: true,
+	}
 })
 
 export default mongoose.model('User', userSchema)
