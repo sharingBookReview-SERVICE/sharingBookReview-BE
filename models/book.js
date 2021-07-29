e8d4a955846204bcee1beea16ca81574485e4812
seungbin0508 <seungbin0508@gmail.com>
2021-07-29 09:28:04 +0900

    Add bookSchema

--- models/book.js
+++ models/book.js
@@ -1,7 +1,18 @@
 import mongoose from 'mongoose'
 
 const bookSchema = new mongoose.Schema({
-
+	_id: {
+		type: Number,
+		alias: 'isbn',
+	},
+	title: String,
+	link: String,
+	image: String,
+	author: String,
+	price: Number,
+	discount: Number,
+	description: String,
+	pubdate: Date,
 })
 
 export default mongoose.model('Book', bookSchema)
