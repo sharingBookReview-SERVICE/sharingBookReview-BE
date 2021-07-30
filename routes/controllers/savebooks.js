e5f3d0b212d0a748f254bc105db5e63104f7f0a2
ohbinkwon <kobbb2100@gmail.com>
2021-07-30 21:49:17 +0900

    [fix] modify savebooks.js

--- routes/controllers/savebooks.js
+++ routes/controllers/savebooks.js
@@ -1,14 +1,14 @@
 import Book from '../../models/book.js'
 
-const saveBooks = async (bookId, book) => {
+const saveBook = async (object, bookId) => {
     const bookExist = await Book.findById(bookId)
     if (bookExist == null){
         const newBook = new Book
-        for (const [key,value] of Object.entries(book)){            
+        for (const [key,value] of Object.entries(object)){            
             newBook[key] = value
             }
         await newBook.save()
         }
 }
 
-export default saveBooks
+export default saveBook
