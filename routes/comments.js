1798ab7141ca8870b80526101f7ec28b86d4fe6b
seungbin0508 <seungbin0508@gmail.com>
2021-07-30 22:51:23 +0900

    Fix missing await keyword

--- routes/comments.js
+++ routes/comments.js
@@ -4,23 +4,19 @@ import { Review, Comment } from '../models/index.js'
 
 const router = new express.Router({ mergeParams: true })
 
-
-
 router.post('/', async (req, res, next) => {
 	const { reviewId } = req.params
 
 	try {
-		const review = Review.findById(reviewId).exec()
+		const comment = Comment.create(req.body)
 
-		if (!review) return next(new Error('존재하지 않는 리뷰 아이디입니다.'))
-
-		const comment = new Comment(req.body)
-
-		await review.comments.push(comment)
-		await review.save()
+		await Review.findByIdAndUpdate(reviewId, {
+			$push: {
+				comments: comment,
+			},
+		})
 
 		return res.sendStatus(201)
-		// todo findByIdAndUpdate(,{$push:}) review 존재하지 않을 때의 에러처리 확인하고 적용하기
 	} catch (e) {
 		return next(new Error('댓글 작성을 실패했습니다.'))
 	}
