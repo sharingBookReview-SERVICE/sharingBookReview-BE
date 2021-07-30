cfd2ff582074931d4331493ada636510651e2d92
ohbinkwon <kobbb2100@gmail.com>
2021-07-30 22:53:26 +0900

    remove !

--- routes/controllers/savebooks.js
+++ routes/controllers/savebooks.js
@@ -1,7 +1,7 @@
 import Book from '../../models/book.js'
 
 const saveBook = async (object, bookId) => {
-    if (!await Book.findById(bookId))return
+    if (await Book.findById(bookId))return
         const newBook = new Book
         for (const [key,value] of Object.entries(object)){            
             newBook[key] = value
