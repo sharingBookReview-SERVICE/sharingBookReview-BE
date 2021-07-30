49b729473784e26708a2077c92e19ab978b0428f
seungbin0508 <seungbin0508@gmail.com>
2021-07-30 21:01:01 +0900

    Prevent pushing a comment to a review that doesn't exist

--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import { Review, Comment } from '../models/index.js'
 
 const router = new express.Router({ mergeParams: true })
 
-router.post('/', (req, res) => {
+const { bookId, reviewId } = req.params
+
+router.post('/', async (req, res, next) => {
+	try {
+		const review = Review.findById(reviewId).exec()
+
+		if (!review) return next(new Error('존재하지 않는 리뷰 아이디입니다.'))
+
+	} catch (e) {
+
+	}
 	return res.sendStatus(201)
 })
 
