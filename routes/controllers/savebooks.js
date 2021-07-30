1955fc2f3bf77d2f54002118f52c0de5c49de585
ohbinkwon <kobbb2100@gmail.com>
2021-07-30 17:54:34 +0900

    make controllers 'savebook.js'

---
+++ routes/controllers/savebooks.js
@@ -0,0 +1,14 @@
+import Book from '../../models/book.js'
+
+const saveBooks = async (bookId) => {
+    const bookExist = await Book.findById(bookId)
+    if (bookExist == null){
+        const newBook = new Book
+        for (const [key,value] of Object.entries(book)){            
+            newBook[key] = value
+            }
+        await newBook.save()
+        }
+}
+
+export default saveBooks
