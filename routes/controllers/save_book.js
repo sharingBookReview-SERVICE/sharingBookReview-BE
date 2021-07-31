89dc730d23676e53a06e63f193fb60a7b45d6705
seungbin0508 <seungbin0508@gmail.com>
2021-07-31 12:07:49 +0900

    Delete redundant logic
    This part is already checked in reviews router

--- routes/controllers/save_book.js
+++ routes/controllers/save_book.js
@@ -1,7 +1,6 @@
 import Book from '../../models/book.js'
 
 const saveBook = async (object, bookId) => {
-	if (await Book.findById(bookId)) return
 	const newBook = new Book()
 	for (const [key, value] of Object.entries(object)) {
 		newBook[key] = value
