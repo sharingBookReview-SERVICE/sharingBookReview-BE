5a331130449d9ffa2b062536ef93a03f3898e06e
seungbin0508 <seungbin0508@gmail.com>
2021-07-31 12:16:03 +0900

    Add jsdoc

--- routes/controllers/save_book.js
+++ routes/controllers/save_book.js
@@ -1,8 +1,16 @@
 import { Book } from '../../models/index.js'
 
-const saveBook = async (object, bookId) => {
+/**
+ * Saves book search result into DB.
+ * @param {Object} searchResult Search result from naver dev api.
+ * @returns {Promise<Document<any, any, unknown>>} book document
+ */
+const saveBook = async (searchResult) => {
 	const newBook = new Book()
-	for (const [key, value] of Object.entries(object)) {
+	/**
+	 * Fix search result's format into DB's format
+	 */
+	for (const [key, value] of Object.entries(searchResult)) {
 		newBook[key] = value
 	}
 	return await newBook.save()
