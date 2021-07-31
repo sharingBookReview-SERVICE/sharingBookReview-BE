fca63fd1c86322bd612f21dfe3f6eee7e5e59169
seungbin0508 <seungbin0508@gmail.com>
2021-07-31 11:30:41 +0900

    Prevent modified path and/or book info

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,9 +6,15 @@ import searchBooks from './controllers/searchbooks.js'
 const router = new express.Router({ mergeParams: true })
 
 router.post('/', async (req, res, next) => {
-    const { bookId } = req.params
-    // const { userId } = req.locals.user   
-    const { quote, content, hashtags, image } = req.body
+	const { bookId } = req.params
+	// const { userId } = req.locals.user
+	const { isbn, quote, content, hashtags, image } = req.body
+
+	if (bookId !== isbn)
+		return next(
+			new Error('URL 상의 책 정보와 실제 책의 정보가 일치하지 않습니다.')
+		)
+
 
 	try {
 		const searchList = await searchBooks('isbn', bookId)
@@ -22,14 +28,11 @@ router.post('/', async (req, res, next) => {
 			image,
 		})
 		await review.save()
-		await Book.findByIdAndUpdate(
-			bookId,
-			{ $push: { reviews: review._id } }
-		)
+		await Book.findByIdAndUpdate(bookId, { $push: { reviews: review._id } })
 
 		return res.sendStatus(201)
 	} catch (e) {
-    	console.error(e)
+		console.error(e)
 		return next(new Error('리뷰 작성을 실패했습니다.'))
 	}
 })
@@ -37,27 +40,29 @@ router.post('/', async (req, res, next) => {
 router.get('/', async (req, res) => {
 	const { bookId } = req.params
 
-    const reviewList = await Review.findById({ bookId })
-    
-	return res.json({reviewList})
+	const reviewList = await Review.findById({ bookId })
+
+	return res.json({ reviewList })
 })
 
 router.get('/:reviewId', async (req, res) => {
-    const { bookId, reviewId } = req.params
+	const { bookId, reviewId } = req.params
 
-    const review = await Review.findById( reviewId )
-    
+	const review = await Review.findById(reviewId)
+
 	return res.json({ review })
 })
 
 router.put('/:reviewId', async (req, res) => {
-    const { bookId, reviewId } = req.params
-    const { quote, content, hashtags, image } = req.body
-    
-    await Review.findByIdAndUpdate(
-        reviewId ,
-        { quote, content, hashtags, image }
-        )
+	const { bookId, reviewId } = req.params
+	const { quote, content, hashtags, image } = req.body
+
+	await Review.findByIdAndUpdate(reviewId, {
+		quote,
+		content,
+		hashtags,
+		image,
+	})
 
 	return res.sendStatus(202)
 })
