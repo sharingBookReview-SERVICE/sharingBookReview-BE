d53ee2a100e21e6c9ff4494a5a4ecc703387b752
YJ <lyjoo7413@naver.com>
2021-08-01 00:36:08 +0900

    Add social-login_google
    Add social-login_google

---
+++ google-login.js
@@ -0,0 +1,19 @@
+var passport = require('passport')
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID:
+				531193406993 -
+				gf1v14ui793k2nv119nq0be6eaqgtt8h.apps.googleusercontent.com,
+			clientSecret: kY3QDE2eRhViy1ehE4yVFmvs,
+			callbackURL: 'http://www.example.com/auth/google/callback',
+		},
+		function (accessToken, refreshToken, profile, done) {
+			User.findOrCreate({ googleId: profile.id }, function (err, user) {
+				return done(err, user)
+			})
+		}
+	)
+)
