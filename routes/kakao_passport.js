4dd164248f714bb5d8d376a2dc96c44682425588
ohbinkwon <kobbb2100@gmail.com>
2021-07-31 22:15:27 +0900

    init kakao_passport

--- routes/kakao_passport.js
+++ routes/kakao_passport.js
@@ -2,3 +2,30 @@ import jwt from "jsonwebtoken";
 import Users from "../schemas/user";
 import passportRouter from "passport";
 import { Strategy } from "passport-kakao";
+
+const kakaoPassportConfig = () => {
+    passportRouter.serializeUser((user, done) => {
+        done(null, user);
+    })
+    
+    passportRouter.deserializeUser((user, done) => {
+        done(null, user);
+    })
+    
+    passportRouter.use(
+		new Strategy(
+			{
+				clientID: '89c020b3b307f237f8e3e3135ce353cf',
+				clientSecret: '',
+				callbackURL: 'http://13.124.63.103/api/users/kakao/callback',
+			},
+			async (_, __, profile, done) => {
+				const providerKey = profile.id
+				const provider = profile.provider
+				
+				return done(null, user)
+			}
+		)
+	)
+}
+export { kakaoPassportConfig };
