af05f773da16073aa6b9e1bb56d474b0a612922d
ohbinkwon <kobbb2100@gmail.com>
2021-08-01 13:06:33 +0900

    sorry prettier

--- routes/kakao_passport.js
+++ routes/kakao_passport.js
@@ -18,20 +18,20 @@ const kakaoPassportConfig = () => {
 				clientID: '89c020b3b307f237f8e3e3135ce353cf',
 				clientSecret: '',
 				callbackURL: 'http://localhost:3000/api/users/kakao/callback',
-                // http://13.124.63.103/api/users/kakao/callback
+				// http://13.124.63.103/api/users/kakao/callback
 			},
 			async (_, __, profile, done) => {
 				const providerKey = profile.id
 				const provider = profile.provider
-                
-                let user = await User.findOne({ provider, providerKey })
-                
-                if(!user){
-                    user = await User.create({ provider, providerKey })
-                }
 
-                const token = jwt.sign({ userId : user._id }, 'ohbinisthebest');
-				
+				let user = await User.findOne({ provider, providerKey })
+
+				if (!user) {
+					user = await User.create({ provider, providerKey })
+				}
+
+				const token = jwt.sign({ userId: user._id }, 'ohbinisthebest')
+
 				return done(null, user, token)
 			}
 		)
