d68420a68d547ce40255de7f5887ab761d05a391
ohbinkwon <kobbb2100@gmail.com>
2021-08-01 14:42:48 +0900

    [userRouter][get reviews] make router

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import jwt from "jsonwebtoken";
 import User from "../models/user.js";
+import Review from "../models/review.js";
 import passport from "passport";
 
 const router = new express.Router()
@@ -95,15 +95,23 @@ router.put('/:userId', async (req, res) => {
 })
 
 router.delete('/:userId', async (req, res) => {
-    const {userId} = req.params
+    const { userId } = req.params
     
     await User.findByIdAndDelete(userId)
 
 	return res.sendStatus(200)
 })
 
-router.get('/:userId/reviews', (req, res) => {
-	return res.json(sampleReview)
+router.get('/:userId/reviews', async (req, res) => {
+    const { userId } = req.params
+    // id에 관한건 confirm merge 후에 findOne안의 인자와 select,populate값 user:userId로 바꿔줘야함
+    // 각 기능 userId 추가 후에 기능 검사 필
+    const reviews = await Review.findOne({ userId }).select('reviews').populate({
+        path: 'reviews',
+        options: { sort: { created_at: -1 } },
+    })
+
+	return res.json(reviews)
 })
 
 export default router
