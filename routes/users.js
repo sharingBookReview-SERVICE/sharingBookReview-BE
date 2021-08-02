b5b3daadfeb6e9a5574e0d6d9e448b791449cabf
ohbinkwon <kobbb2100@gmail.com>
2021-08-02 11:32:20 +0900

    change findbyId update and delete

--- routes/users.js
+++ routes/users.js
@@ -24,11 +24,9 @@ router.put("/nickname/:userId", async (req,res,next) => {
     
     try{		
 		// findByIdAndUpdate를 사용하면 user가 존재하지 않아도 등록된것으로 인식하기 때문에 나누어서 error 처리
-		const user = await User.findById(userId)
-		if (!user) {
-			return next(new Error('등록되지 않은 유저입니다'))
-		}
-		await user.updateOne({ nickname })
+		const user = await User.findByIdAndUpdate(userId,{nickname})
+
+        if (user == null)return next(new Error('등록되지 않은 유저입니다'))
 
 		return res.sendStatus(200)
 
@@ -56,12 +54,10 @@ router.put('/:userId', async (req, res, next) => {
     const { userId } = req.params
     const { nickname } = req.body
     try{
-        const user = await User.findById(userId)
-		if (!user) {
-			return next(new Error('등록되지 않은 유저입니다'))
-		}
-		await user.updateOne({ nickname })
-
+        const user = await User.findByIdAndUpdate(userId,{nickname})
+		
+        if (user == null)return next(new Error('등록되지 않은 유저입니다'))
+		
         return res.sendStatus(200)
 
     } catch(e) {
@@ -74,11 +70,9 @@ router.put('/:userId', async (req, res, next) => {
 router.delete('/:userId', async (req, res, next) => {
     const { userId } = req.params
     try{
-        const user = await User.findById(userId)
-        if (!user) {
-			return next(new Error('등록되지 않은 유저입니다'))
-		}
-        await user.deleteOne()
+        const user = await User.findByIdAndDelete(userId)
+        
+        if (user == null)return next(new Error('등록되지 않은 유저입니다'))
 
         return res.sendStatus(200)
 
@@ -90,23 +84,21 @@ router.delete('/:userId', async (req, res, next) => {
 })
 
 router.get('/:userId/reviews', async (req, res, next) => {
-    const { userId } = req.params
-    // id에 관한건 confirm merge 후에 findOne안의 인자와 select,populate값 user:userId로 바꿔줘야함
-    // 각 기능 userId 추가 후에 기능 검사 필
-    try{
-        const reviews = await Review.findOne({ userId })
+	const { userId } = req.params
+	// id에 관한건 confirm merge 후에 findOne안의 인자와 select,populate값 user:userId로 바꿔줘야함
+	// 각 기능 userId 추가 후에 기능 검사 필
+	try {
+		const reviews = await Review.findOne({ userId })
 			.select('reviews')
 			.populate({
 				path: 'reviews',
 				options: { sort: { created_at: -1 } },
 			})
-    
-        return res.json(reviews)
 
-    } catch(e) {
-        return next(new Error('nickname 등록을 실패했습니다.'))
-    }
-    
+		return res.json(reviews)
+	} catch (e) {
+		return next(new Error('nickname 등록을 실패했습니다.'))
+	}
 })
 
 export default router
