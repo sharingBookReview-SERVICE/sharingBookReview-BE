fa5c9880be2ee2273565d6b6c79775899efabae8
YJ <lyjoo7413@naver.com>
2021-08-02 20:30:22 +0900

    add> google_id  받아옴.

--- routes/google_passport.js
+++ routes/google_passport.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const passport = require('passport')
-const session = require('express-session'),
-	bodyParser = require('body-parser')
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 
 const googleCredentials = require('./config/google.json')
@@ -11,9 +9,11 @@ passport.use(
 		{
 			clientID: googleCredentials.web.client_id,
 			clientSecret: googleCredentials.web.client_secret,
-			callbackURL: googleCredentials.web.redirect_uris,
+			callbackURL: googleCredentials.web.redirect_uris[0],
 		},
 		function (accessToken, refreshToken, profile, done) {
+			// DB에서 User 정보(porfile)를 받아옴.
+			console.log('GoogleStrategy', accessToken, refreshToken, profile)
 			User.findOrCreate({ googleId: profile.id }, function (err, user) {
 				return done(err, user)
 			})
@@ -21,9 +21,19 @@ passport.use(
 	)
 )
 
+// 인증서 & scope 설정
 app.get(
 	'/auth/google',
 	passport.authenticate('google', {
 		scope: ['https://www.googleapis.com/auth/plus.login'],
 	})
+)
+
+// 임시 코드 발행
+app.get(
+	'/auth/google/callback',
+	passport.authenticate('google', { failureRedirect: '/auth/login' }),
+	function (req, res) {
+		res.redirect('/')
+	}
 )
