34f24d016df49fd68405a46fe7fe83c0d1252019
ohbinkwon <kobbb2100@gmail.com>
2021-08-04 14:33:29 +0900

    set review image controllers

--- routes/controllers/review_image.js
+++ routes/controllers/review_image.js
@@ -1,5 +1,13 @@
 const reviewImage = {
+    uploadImage: async( req, res ) => {
+        const image = req.file.path
+        
+        if (image === undefined) {
+            return next(new Error("이미지가 존재하지 않습니다."))
+        }
 
+        res.sendStatus(200)
+    }
 
 }
 
