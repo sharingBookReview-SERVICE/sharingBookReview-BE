5e91aff6c703b49ddf5c52126dfc1ccced8b6bc0
SeungBin Kim <seungbin0508@gmail.com>
2021-08-05 10:53:40 +0900

    Apply prettier

--- routes/feeds.js
+++ routes/feeds.js
@@ -2,16 +2,15 @@ import express from 'express'
 import { Review } from '../models/index.js'
 
 const router = new express.Router()
-    router.get('/', async (req, res, next) => {
-        try {
-			const feeds = await Review.find({}).populate('book').sort('-created_at')
 
-			return res.json({ feeds })
-		} catch (err) {
-			return next(new Error('피드를 불러오는데 실패했습니다.'))
-		}
+router.get('/', async (req, res, next) => {
+	try {
+		const feeds = await Review.find({}).populate('book').sort('-created_at')
+
+		return res.json({ feeds })
+	} catch (err) {
+		return next(new Error('피드를 불러오는데 실패했습니다.'))
+	}
 })
-
-
 
 export default router
