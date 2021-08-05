0a9e0d1d679361a62184c3ccce67bf2348d24d50
SeungBin Kim <seungbin0508@gmail.com>
2021-08-05 10:58:49 +0900

    Add likes info in reviews

--- routes/feeds.js
+++ routes/feeds.js
@@ -4,10 +4,15 @@ import { Review } from '../models/index.js'
 const router = new express.Router()
 
 router.get('/', async (req, res, next) => {
-	try {
-		const feeds = await Review.find({}).populate('book').sort('-created_at')
 
-		return res.json({ feeds })
+	const userId = 'temp' //todo 로그인 안 된 상태에서 어떻게 처리할지 정해야함
+
+	try {
+		const reviews = await Review.find({}).populate('book').sort('-created_at')
+
+		const result = reviews.map(review => Review.processLikesInfo(review, userId))
+		return res.json({ reviews: result })
+
 	} catch (err) {
 		return next(new Error('피드를 불러오는데 실패했습니다.'))
 	}
