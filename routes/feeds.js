1788790af5a5e2982ab57dd0f3f91f6e4d90d7bc
SeungBin Kim <seungbin0508@gmail.com>
2021-08-05 17:06:36 +0900

    Fix feeds router to populate user

--- routes/feeds.js
+++ routes/feeds.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res, next) => {
 	const userId = 'temp' //todo 로그인 안 된 상태에서 어떻게 처리할지 정해야함
 
 	try {
-		const reviews = await Review.find({}).populate('book').sort('-created_at')
+		const reviews = await Review.find({}).populate('book user').sort('-created_at')
 
 		const result = reviews.map(review => Review.processLikesInfo(review, userId))
 		return res.json({ reviews: result })
