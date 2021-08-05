2ab150080bd407234d341d66954c5ef53c0b60cb
SeungBin Kim <seungbin0508@gmail.com>
2021-08-05 22:36:05 +0900

    Fix !authorization to return res.locals.user with 'non-login' as id

--- middleware/auth_middleware.js
+++ middleware/auth_middleware.js
@@ -8,9 +8,11 @@ const authMiddleware = (req, res, next) => {
 		const tokenScheme = authorization.split(' ')[0]
 		const tokenValue = authorization.split(' ')[1]
 
-		if (authorization === null || authorization === undefined) {
-			return next(new Error('로그인을 위한 정보가 존재 하지 않습니다.'))
+		if (!authorization) {
+			res.locals.user._id = 'non-login'
+			return next()
 		}
+
 		if (tokenScheme !== 'Bearer') {
 			return next(new Error('토큰 인증 방식이 잘못되었습니다.'))
 		}
@@ -26,6 +28,7 @@ const authMiddleware = (req, res, next) => {
 			.catch((e) => {
 				return next(new Error('유저 정보가 존재하지 않습니다.'))
 			})
+
 	} catch (e) {
 		return next(new Error('인증에 실패했습니다.'))
 	}
