5088729a375971cd746dc9ce019d12a186d6415d
SeungBin Kim <seungbin0508@gmail.com>
2021-08-05 22:28:45 +0900

    Add getChanges

--- controllers/index_tags.js
+++ controllers/index_tags.js
@@ -1,6 +1,22 @@
-import { ChangeIndex } from '../models/index.js'
+import { Book, ChangeIndex } from '../models/index.js'
 import schedule from 'node-schedule'
 
-const job = schedule.scheduleJob('0 * * * * *', () => {
-	console.log('Hello World!')
+const getChanges = async () => {
+
+	const changes = await ChangeIndex.find({})
+
+	await Promise.allSettled([...changes.map(change => {
+		change.indexed = true
+		return change.save()
+	})])
+
+	return new Set(changes.map(change => change.isbn))
+}
+
+const job = schedule.scheduleJob('0 * * * * *', async () => {
+	await ChangeIndex.create({isbn: '1234'}) // temporary document.
+	const changedISBNs = await getChanges()
+	await ChangeIndex.deleteMany({indexed: true})
+	console.log(changedISBNs)
 })
+
