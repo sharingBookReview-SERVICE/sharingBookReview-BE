50f5fa8b5ac79ece3329016c8225b8fb29dcf9df
SeungBin Kim <seungbin0508@gmail.com>
2021-08-07 01:37:52 +0900

    Update getChanges function
    Add jsdoc and apply prettier

--- controllers/index_tags.js
+++ controllers/index_tags.js
@@ -5,30 +5,40 @@
 import { Book, ChangeIndex } from '../models/index.js'
 import schedule from 'node-schedule'
 
+/**
+ * Returns unique isbns which have changed after the last run.
+ * Marks returned documents' indexed property as true, so it can be deleted later.
+ * @returns {Promise<Set<Number>>}
+ */
 const getChanges = async () => {
 
 	const changes = await ChangeIndex.find({})
 
-	await Promise.allSettled([...changes.map(change => {
-		change.indexed = true
-		return change.save()
-	})])
+	await Promise.allSettled([
+		...changes.map(change => {
+			change.indexed = true
+			return change.save()
+		})])
 
 	return new Set(changes.map(change => change.isbn))
 }
 
 const job = schedule.scheduleJob('0 * * * * *', async () => {
-	await ChangeIndex.create({isbn: '1234'}) // temporary document.
+	const input = await Book.findOne()
+	console.log(input._id)
+	await ChangeIndex.create({isbn: input._id})
 	const changedISBNs = await getChanges()
 	await ChangeIndex.deleteMany({indexed: true})
 
 	// Books to be indexed
-	const books = Book.find({
+	const books = await Book.find({
 		_id: {
 			$in: changedISBNs,
 		},
 	})
 
-	console.log(books)
+	books.forEach((book) => {
+		console.log(book)
+	})
 })
 
