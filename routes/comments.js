5fe10c2b38ec6fb22d8ca3b98e863bae90d199a4
SeungBin Kim <seungbin0508@gmail.com>
2021-08-09 11:40:41 +0900

    Update DELETE /:commentId

--- routes/comments.js
+++ routes/comments.js
@@ -65,18 +65,15 @@ router.delete('/:commentId', authMiddleware, async (req, res, next) => {
 		const review = await Review.findById(reviewId)
 		const comment = review.comments.id(commentId)
 
-		if (comment === null)
-			return next(new Error('댓글이 존재하지 않습니다.'))
-		if (String(comment.user) !== String(userId))
-			return next(
-				new Error('댓글 작성자와 현재 로그인된 사용자가 다릅니다.')
-			)
+		if (!comment) return next(new Error('삭제하려는 댓글이 존재하지 않습니다.'))
+		if (String(comment.user) !== String(userId)) return next(new Error('댓글 작성자와 토큰에 담긴 사용자의 정보가 일치하지 않습니다.'))
 
 		await review.comments.pull(commentId)
 		await review.save()
 
 		return res.sendStatus(204)
 	} catch (e) {
+		console.error(e)
 		return next(new Error('댓글 삭제를 실패했습니다.'))
 	}
 })
