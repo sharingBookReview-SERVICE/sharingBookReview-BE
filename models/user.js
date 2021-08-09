36b83fcb29f9c60e0b38ad6379a31da5851c634d
ohbin-kwon <ohbinkwon.dev@gmail.com>
2021-08-09 22:51:58 +0900

    add explanation

--- models/user.js
+++ models/user.js
@@ -27,13 +27,15 @@ const userSchema = new mongoose.Schema({
 // why 누가 어디에 event를 실행했는지를 저장하는것이 확장성이 높다.
 userSchema.statics.getExpAndLevelUp = async function(userId, event) {
     const user = await this.findById(userId)
+    // event에 따른 경험치를 획득
     user.exp += expList[event]
+    // 필요 경험치 계산
     let requiredExp = 3 * (1.05**(user.level - 1))
-
+    // 현재 경험치가 필요 경험치보다 크거나 같을때, 레벨업, 경험치 수정
     if(user.exp >= requiredExp){
         user.level += 1
         user.exp -= requiredExp
-}
+    }
         await user.save()
 
         return user
