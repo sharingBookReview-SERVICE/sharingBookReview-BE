a745b3cc7be5fd82e37cad6771abeb1ad93f930c
SeungBin Kim <seungbin0508@gmail.com>
2021-08-11 11:36:17 +0900

    Update feeds to split documents into 10 items

--- routes/feeds.js
+++ routes/feeds.js
@@ -7,18 +7,40 @@ router.get('/', async (req, res, next) => {
 	const SCROLL_SIZE = 10
 	const { lastItemId } = req.body
 
-	const userId = 'temp' //todo 로그인 안 된 상태에서 어떻게 처리할지 정해야함
-	// Set page as 1 if not specified by the client
-
 	try {
-		const reviews = await Review.find({}).populate('book user').sort('-created_at')
+		let reviews
 
-		const result = reviews.map(review => Review.processLikesInfo(review, userId))
-		return res.json(result)
+		if (!lastItemId) {
+			reviews = await Review.find()
+			.sort('-created_at')
+			.limit(SCROLL_SIZE)
+			.populate('book user')
+		} else {
+			reviews = await Review.find()
+			.sort('-created_at')
+			.where('_id')
+			.lt(lastItemId)
+			.limit(SCROLL_SIZE)
+			.populate('book user')
+		}
 
-	} catch (err) {
-		return next(new Error('피드를 불러오는데 실패했습니다.'))
+		return res.json({reviews})
+	} catch (e) {
+		console.error(e)
+		return next(new Error('피드 불러오기를 실패했습니다.'))
 	}
+
+
+	// const userId = 'temp' //todo 로그인 안 된 상태에서 어떻게 처리할지 정해야함
+	// try {
+	// 	const reviews = await Review.find({}).populate('book user').sort('-created_at')
+	//
+	// 	const result = reviews.map(review => Review.processLikesInfo(review, userId))
+	// 	return res.json(result)
+	//
+	// } catch (err) {
+	// 	return next(new Error('피드를 불러오는데 실패했습니다.'))
+	// }
 })
 
 export default router
