1aabe554041cb3a123950b5e2dfb9f7b73e0a61f
SeungBin Kim <seungbin0508@gmail.com>
2021-08-11 20:49:20 +0900

    Add contents property and fix comments property

--- models/collection.js
+++ models/collection.js
@@ -1,6 +1,17 @@
 import mongoose from 'mongoose'
 import { commentSchema } from './comment.js'
 
+const contentSchema = new mongoose.Schema({
+	book: {
+		type: [mongoose.Schema.Types.ObjectId],
+		ref: 'Book',
+		required: true,
+	},
+	book_description: {
+		type: String,
+	},
+})
+
 const collectionSchema = new mongoose.Schema({
 	name: {
 		type: String,
@@ -18,11 +29,10 @@ const collectionSchema = new mongoose.Schema({
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'User',
 	},
+	contents: [contentSchema],
 	image: String,
 	description: String,
-	comments: {
-		type: [commentSchema]
-	},
+	comments: [commentSchema],
 	liked_users: {
 		type: [mongoose.Schema.Types.ObjectId],
 		ref: 'User',
