0352fb9ab2fbf0332e53e5dd741d6fe8b1d637b6
ohbin-kwon <ohbinkwon.dev@gmail.com>
2021-08-11 12:14:31 +0900

    add get router following list and follower list

--- routes/follow.js
+++ routes/follow.js
@@ -5,13 +5,33 @@ import { Follow, User } from '../models/index.js'
 const router = new express.Router()
 
 // 팔로잉 목록 조회
-router.get('/', async (req, res, next) => {
+router.get('/followingList', authMiddleware, async (req, res, next) => {
+    try{
+        const { _id : userId } = res.locals.user
 
+        const followList = await Follow.find({sender : userId}).populate({path : 'receiver', select : 'level _id nickname'})
+        const followingList = followList.map((follow) => {
+            return follow.receiver
+        })
+        res.json({followingList})
+    }catch(e){
+        return next(new Error('팔로잉 리스트 불러오기를 실패했습니다.'))
+    }
 })
 
-// 팔로우 목록 조회
-router.get('/', async (req, res, next) => {
+// 팔로워 목록 조회
+router.get('/followerList', authMiddleware, async (req, res, next) => {
+    try{
+        const { _id : userId } = res.locals.user
 
+        const followList = await Follow.find({receiver : userId}).populate({path : 'sender', select : 'level _id nickname'})
+        const followerList = followList.map((follow) => {
+            return follow.sender
+        })
+        res.json({followerList})
+    }catch(e){
+        return next(new Error('팔로잉 리스트 불러오기를 실패했습니다.'))
+    }
 })
 
 // switch follow
