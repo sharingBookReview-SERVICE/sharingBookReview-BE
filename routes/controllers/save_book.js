98063b4d0271bfb27144ce9788e47cf702b3da3e
ohbin-kwon <ohbinkwon.dev@gmail.com>
2021-08-12 03:04:53 +0900

    move to save books
    need to solve problem of tags

--- routes/controllers/save_book.js
+++ routes/controllers/save_book.js
@@ -1,4 +1,10 @@
 import { Book } from '../../models/index.js'
+import getDescription from './book_detail_crawling.js'
+
+// else if(key == "description"){
+//     const description = await getDescription(array[i].link[0])
+//     book[key] = description
+// }
 
 /**
  * Saves book search result into DB.
@@ -11,7 +17,13 @@ const saveBook = async (searchResult) => {
 	 * Fix search result's format into DB's format
 	 */
 	for (const [key, value] of Object.entries(searchResult)) {
-		newBook[key] = value
+        if (key == 'description'){
+            const description = await getDescription(searchResult.link)
+            newBook[key] = description
+        }else{
+            newBook[key] = value
+        }
+		
 	}
 	return await newBook.save()
 }
