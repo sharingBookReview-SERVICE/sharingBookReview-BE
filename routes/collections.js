fdb4596b6ea4a69008f492e6f00c9063f4b0a127
SeungBin Kim <seungbin0508@gmail.com>
2021-08-11 21:47:17 +0900

    Add POST /

--- routes/collections.js
+++ routes/collections.js
@@ -1,6 +1,25 @@
 import express from 'express'
-
+import { Collection } from '../models/index.js'
+import authMiddleware from '../middleware/auth_middleware.js'
 const router = new express.Router()
+
+router.post('/', authMiddleware ,async (req, res) => {
+	const { _id: userId } = res.locals.user
+
+	try {
+		const collection = await Collection.create({
+			...req.body,
+			type: 'custom',
+			user: userId,
+		})
+
+		return res.json({collection})
+
+	} catch (e) {
+		console.error(e)
+		return next(new Error('컬렉션 작성을 실패했습니다.'))
+	}
+})
 
 router.get('/', (req, res) => {
 
