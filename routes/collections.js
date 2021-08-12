e6067d5fafba8711f13d5edd37e9902532c4fe7d
ohbin-kwon <ohbinkwon.dev@gmail.com>
2021-08-12 19:09:50 +0900

    upload image

--- routes/collections.js
+++ routes/collections.js
@@ -14,12 +14,11 @@ router.use(authMiddleware)
 
 router.post('/', upload.single('image'), ImageUpload.uploadImage, async (req, res, next) => {
 	const { _id: userId } = res.locals.user
-    const { bookId } = req.params
     const image = res.locals?.url
     const { name, description } = req.body
-    const contents = JSON.parse(req.body.hashtags)
+    const contents = JSON.parse(req.body.contents)
 	try {
-		const collection = await Collection.create({ ...req.body, type: 'custom', user: userId })
+		const collection = await Collection.create({ image, name, description, contents, type: 'custom', user: userId })
 
 		return res.status(201).json({collection})
 	} catch (e) {
