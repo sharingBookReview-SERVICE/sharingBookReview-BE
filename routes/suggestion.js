92b13879cd9d11a68d6349252a3e88540b02834a
SeungBin Kim <seungbin0508@gmail.com>
2021-08-12 14:12:15 +0900

    Apply prettier

--- routes/suggestion.js
+++ routes/suggestion.js
@@ -5,15 +5,14 @@ import { Suggestion, User } from '../models/index.js'
 const router = new express.Router()
 
 router.post('/', authMiddleware, async (req, res, next) => {
-    try{
-        const { _id : userId } = res.locals.user
-    
-        await Suggestion.create({userId, ...req.body})
-        res.json('success')
-    }catch(e){
-        next(new Error("건의 사항 등록에 실패했습니다."))
-    }
-    
+	try {
+		const { _id: userId } = res.locals.user
+
+		await Suggestion.create({ userId, ...req.body })
+		res.json('success')
+	} catch (e) {
+		next(new Error('건의 사항 등록에 실패했습니다.'))
+	}
 })
 
 export default router
