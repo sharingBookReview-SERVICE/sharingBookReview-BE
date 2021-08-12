8a450f5b4b477e34d9acb756c8aecab159e13cc0
SeungBin Kim <seungbin0508@gmail.com>
2021-08-12 16:28:35 +0900

    Complete POST /collections

--- routes/collections.js
+++ routes/collections.js
@@ -3,6 +3,18 @@ import { Collection } from '../models/index.js'
 import authMiddleware from '../middleware/auth_middleware.js'
 const router = new express.Router()
 
+router.post('/', authMiddleware, async (req, res, next) => {
+	const { _id: userId } = res.locals.user
+	try {
+		const collection = await Collection.create({ ...req.body, type: 'custom', user: userId })
+
+		return res.status(201).json({collection})
+	} catch (e) {
+		console.error(e)
+		return next(new Error('컬렉션 작성을 실패했습니다.'))
+	}
+})
+
 // Get all collections
 router.get('/', authMiddleware, async (req, res, next) => {
 	try {
@@ -14,28 +26,6 @@ router.get('/', authMiddleware, async (req, res, next) => {
 		console.error(e)
 		return next(new Error('콜렉션 불러오기를 실패했습니다.'))
 	}
-})
-
-router.post('/', authMiddleware ,async (req, res) => {
-	const { _id: userId } = res.locals.user
-
-	try {
-		const collection = await Collection.create({
-			...req.body,
-			type: 'custom',
-			user: userId,
-		})
-
-		return res.json({collection})
-
-	} catch (e) {
-		console.error(e)
-		return next(new Error('컬렉션 작성을 실패했습니다.'))
-	}
-})
-
-router.get('/', (req, res) => {
-
 })
 
 export default router
