5349947f7591d3a0ef0bd39e653a9b2cf5ce6d7e
SeungBin Kim <seungbin0508@gmail.com>
2021-08-13 17:55:10 +0900

    Update user feeds

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { User, Review } from '../models/index.js'
+import { User, Review, Collection } from '../models/index.js'
 import passport from 'passport'
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
@@ -123,21 +123,18 @@ router.delete('/:userId', async (req, res, next) => {
 	}
 })
 
-router.get('/:userId/reviews', async (req, res, next) => {
+// Returns all reviews and collections made by a user
+router.get('/:userId/feeds', async (req, res, next) => {
 	const { userId } = req.params
-	// id에 관한건 confirm merge 후에 findOne안의 인자와 select,populate값 user:userId로 바꿔줘야함
-	// 각 기능 userId 추가 후에 기능 검사 필
-	try {
-		const reviews = await Review.findOne({ userId })
-			.select('reviews')
-			.populate({
-				path: 'reviews',
-				options: { sort: { created_at: -1 } },
-			})
 
-		return res.json(reviews)
+	try {
+		const reviews = await Review.find({user: userId}).sort('-created_at')
+		const collections = await Collection.find({user: userId}).sort('-created_at')
+
+		return res.json({reviews, collections})
 	} catch (e) {
-		return next(new Error('nickname 등록을 실패했습니다.'))
+		console.error(e)
+		return next(new Error('개인 피드 불러오기를 실패했습니다.'))
 	}
 })
 // 프로필 이미지 획득
