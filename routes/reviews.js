e1c89ffc7a75eb55b86eb6671a9ccab54a7bd336
SeungBin Kim <seungbin0508@gmail.com>
2021-08-16 15:46:17 +0900

    Refactor PUT /likes to use abstract function

--- routes/reviews.js
+++ routes/reviews.js
@@ -150,20 +150,33 @@ router.delete('/:reviewId', authMiddleware, async (req, res) => {
 	}
 })
 
+const likeUnlike = async (Model, documentId, userId) => {
+	try {
+		let document =
+			(await Model.findById(documentId)) ??
+			new Error('존재하지 않는 리뷰입니다.')
+
+		await User.getExpAndLevelUp(document.user, 'like')
+
+		document.getMyLike(userId)
+			? document.liked_users.pull(userId)
+			: document.liked_users.push(userId)
+
+		await document.save()
+
+		document = Model.processLikesInfo(document, userId)
+		return document
+	} catch (e) {
+		return e
+	}
+}
+
 router.put('/:reviewId/likes', authMiddleware, async (req, res, next) => {
 	const { _id: userId } = res.locals.user
 	const { reviewId } = req.params
     
     try{
-        let review = await Review.findById(reviewId) ?? next(new Error('존재하지 않는 리뷰입니다.'))
-
-        await User.getExpAndLevelUp(review.user, "like")
-
-		review.getMyLike(userId) ? review.liked_users.pull(userId) : review.liked_users.push(userId)
-		await review.save()
-
-		review = Review.processLikesInfo(review, userId)
-
+        const review = await likeUnlike(Review, reviewId, userId)
 		return res.json({ review })
 	} catch (e) {
 		console.error(e)
