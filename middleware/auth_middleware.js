4951c1347875651e6be42f5b2d770572c0b2adec
ohbin-kwon <ohbinkwon.dev@gmail.com>
2021-08-17 23:58:10 +0900

    change and intergrating auth middleware

--- middleware/auth_middleware.js
+++ middleware/auth_middleware.js
@@ -2,31 +2,37 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/index.js";
 
 const authMiddleware = (req, res, next) => {
+    const { authorization } = req.headers;
+    const tokenScheme = authorization.split(" ")[0]
+    const tokenValue = authorization.split(" ")[1]
+
     try{
-		const { authorization } = req.headers
-
-		const tokenScheme = authorization.split(' ')[0]
-		const tokenValue = authorization.split(' ')[1]
-
-		if (authorization === null || authorization === undefined || tokenValue === 'null') {
-			return next(new Error('로그인을 위한 정보가 존재 하지 않습니다.'))
-		}
-		if (tokenScheme !== 'Bearer') {
+        if (authorization === undefined || authorization === null || tokenValue === 'null'){
+            res.locals.auth = false
+            return next()
+        }
+    }catch(e){
+        return next(new Error("비로그인 인증에 실패했습니다."))
+    }
+    
+    try{    
+        if (tokenScheme !== 'Bearer') {
 			return next(new Error('토큰 인증 방식이 잘못되었습니다.'))
 		}
-
-		const user = jwt.verify(tokenValue, process.env.TOKEN_KEY)
-		const { userId } = user
-
-		User.findById(userId)
-			.then((result) => {
-				res.locals.user = result
-				next()
-			})
-			.catch((e) => {
-				return next(new Error('유저 정보가 존재하지 않습니다.'))
-			})
-	}catch (e){
+    
+        const user = jwt.verify(tokenValue, process.env.TOKEN_KEY)
+        const { userId } = user
+    
+        User.findById(userId)
+        .then((result) => {
+            res.locals.auth = true
+            res.locals.user = result
+            return next()
+        })
+        .catch((e) => {
+            return next(new Error("유저 정보가 존재하지 않습니다."))
+        })
+    }catch (e){
         return next(new Error("인증에 실패했습니다."))
     }
     
