7029b6033811f9da43a4725d278b8a898d37b2e7
ohbin-kwon <ohbinkwon.dev@gmail.com>
2021-08-19 22:28:35 +0900

    init socket io

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ import './controllers/schedule_job.js'
 import router from './routes/index.js'
 import kakaoPassportConfig from "./routes/kakao_passport.js";
 import googlePassportConfig from './routes/google_passport.js'
-
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 config()
 
 const app = new express()
+const server = createServer(app);
 
 app.set('port', process.env.PORT)
 
@@ -20,6 +22,22 @@ app.use(router)
 kakaoPassportConfig()
 googlePassportConfig()
 
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+})
+
+io.on("connection", (socket) => {
+    console.log('what is socket: ', socket);
+    console.log("Socket is active to ve connected");
+
+    socket.on("chat", (payload) => {
+        console.log("what is payload", payload)
+        io.emit("chat", payload)
+    })
+})
 
 app.use((req, res, next) => {
 	const error = new Error(`${req.method} ${req.url} router doesn't exist`)
@@ -32,6 +50,6 @@ app.use((err, req, res, next) => {
 	return res.status(400).json({ error: err.message })
 })
 
-app.listen(app.get('port') || 3000, () => {
+server.listen(app.get('port') || 3000, () => {
 	console.log(`Server listening on port ${app.get('port')}`)
 })
