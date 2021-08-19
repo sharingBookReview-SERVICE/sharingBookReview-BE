b4dcfeac8a718376e316facc00b36bb05df1c148
ohbin-kwon <ohbinkwon.dev@gmail.com>
2021-08-19 11:36:26 +0900

    non auth middleware

--- middleware/non_auth_middleware.js
+++ middleware/non_auth_middleware.js
@@ -3,11 +3,9 @@ import { User } from "../models/index.js";
 
 const authMiddleware = (req, res, next) => {
     const { authorization } = req.headers;
-    const tokenScheme = authorization.split(" ")[0]
-    const tokenValue = authorization.split(" ")[1]
 
     try{
-        if (authorization === undefined || authorization === null || tokenValue === 'null'){
+        if (authorization === undefined || authorization === null){
             let result = new Object
             result._id = 'nonLogin'
             res.locals.user = result
@@ -16,10 +14,16 @@ const authMiddleware = (req, res, next) => {
     }catch(e){
         return next(new Error("비로그인 인증에 실패했습니다."))
     }
+
+    const tokenScheme = authorization.split(" ")[0]
+    const tokenValue = authorization.split(" ")[1]
     
     try{    
         if (tokenScheme !== 'Bearer') {
 			return next(new Error('토큰 인증 방식이 잘못되었습니다.'))
+		}
+        if (tokenValue !== 'Null') {
+			return next(new Error('토큰 벨류가 존재하지않습니다.'))
 		}
     
         const user = jwt.verify(tokenValue, process.env.TOKEN_KEY)
