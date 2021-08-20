6ac0bed82af73856197f72ad6655cc119df8455b
ohbin-kwon <ohbinkwon.dev@gmail.com>
2021-08-20 21:18:58 +0900

    description

--- models/utilities.js
+++ models/utilities.js
@@ -35,6 +35,8 @@ const likeUnlike = (Model, parameterName) => {
 	}
 }
 
+// virtual로 한국시간을 만들고, 현재부터 얼마나 지났는지를 fromNow로 보여줌
+// import 'moment/locale/ko.js' 를 이용해 한국어를 지원
 const KoreaTime = (schema) => {
     schema.virtual('koreaTime').get(function() {
         return moment.tz(this.created_at, "Asia/Seoul").fromNow()
