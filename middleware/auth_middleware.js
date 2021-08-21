ea59d508c6fabe0d25318cfa07a5558112c8a780
SeungBin Kim <seungbin0508@gmail.com>
2021-08-21 16:50:32 +0900

    Add middleware

--- middleware/auth_middleware.js
+++ middleware/auth_middleware.js
@@ -1,6 +1,11 @@
 import jwt from 'jsonwebtoken'
 import { User } from '../models/index.js'
 
+/**
+ * Returns middleware that validate JWT to check whether user is logged in
+ * @param loginRequired {boolean} If false, treat the user as not logged in
+ * @returns {(function(*=, *, *): Promise<*|undefined>)|*}
+ */
 const authMiddleware = (loginRequired) => {
 	/**
 	 * Validates headers from client.
@@ -36,6 +41,29 @@ const authMiddleware = (loginRequired) => {
 		}
 
 		return userId
+	}
+
+	return async (req, res, next) => {
+		try {
+			const userId = validateToken(req)
+
+			const user = await User.findById(userId)
+
+			if (!user)
+				new Error('DB에 JWT 토큰과 일치하는 유저의 정보가 없습니다.')
+
+			res.locals.user = user
+			return next()
+		} catch (e) {
+			console.error(e)
+
+			if (!loginRequired) {
+				res.locals.user = null
+				return next()
+			}
+
+			return next(e)
+		}
 	}
 }
 
