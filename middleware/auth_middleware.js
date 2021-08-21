939ebbb85b0ef86dde8d41b65f68345519c2c7cd
SeungBin Kim <seungbin0508@gmail.com>
2021-08-21 16:42:00 +0900

    Apply prettier

--- middleware/auth_middleware.js
+++ middleware/auth_middleware.js
@@ -1,5 +1,6 @@
-import jwt from "jsonwebtoken";
-import { User } from "../models/index.js";
+import jwt from 'jsonwebtoken'
+import { User } from '../models/index.js'
+
 
 const authMiddleware = (req, res, next) => {
     try{
