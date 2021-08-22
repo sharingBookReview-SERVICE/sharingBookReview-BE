f1092abe660983fdb674de37447dd66fc6dd729f
SeungBin Kim <seungbin0508@gmail.com>
2021-08-23 03:27:38 +0900

    Refactor collection.js

--- models/collection.js
+++ models/collection.js
@@ -1,52 +1,29 @@
-import mongoose from 'mongoose'
+import { Schema, Types, model } from 'mongoose'
 import { commentSchema } from './comment.js'
 import { KoreaTime } from './utilities.js'
 
-const collectionSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
+const collectionSchema = new Schema({
+	name: { type: String, required: true },
 	type: {
 		type: String,
 		enum: ['tag', 'custom', 'best', 'genre'],
 		required: true,
 		immutable: true,
 	},
-	user: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		immutable: true,
-	},
-	contents: [{
-		book: {
-			type: String,
-			ref: 'Book',
-			required: true,
-			alias: 'isbn',
+	user: { type: Types.ObjectId, ref: 'User', immutable: true },
+	contents: [
+		{
+			book: { type: String, ref: 'Book', required: true, alias: 'isbn' },
+			book_description: String,
 		},
-		book_description: String,
-	}],
+	],
 	image: String,
 	description: String,
 	comments: [commentSchema],
-	liked_users: {
-		type: [mongoose.Schema.Types.ObjectId],
-		ref: 'User',
-	},
-	likeCount: {
-		type: Number,
-		default: 0,
-	},
-	created_at: {
-		type: Date,
-		default: Date.now,
-		immutable: true
-	},
-	commented_users:{
-		type: [mongoose.Schema.Types.ObjectId],
-		ref: 'User',
-	},
+	liked_users: { type: [Types.ObjectId], ref: 'User' },
+	likeCount: { type: Number, default: 0 },
+	created_at: { type: Date, default: Date.now, immutable: true },
+	commented_users: { type: [Types.ObjectId], ref: 'User' },
 })
 
 KoreaTime(collectionSchema)
@@ -55,4 +32,4 @@ collectionSchema.pre('save', function () {
 	this.likeCount = this.liked_users.length
 })
 
-export default mongoose.model('Collection', collectionSchema)
+export default model('Collection', collectionSchema)
