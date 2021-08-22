a3b1eb3921579a8e70c2269b5173219d1ac0538e
SeungBin Kim <seungbin0508@gmail.com>
2021-08-23 03:23:25 +0900

    Refactor changes_index.js

--- models/changes_index.js
+++ models/changes_index.js
@@ -1,14 +1,12 @@
-import mongoose from 'mongoose'
+import { Schema, model } from 'mongoose'
 import { KoreaTime } from './utilities.js'
 
-const changesIndexSchema = new mongoose.Schema({
+const changesIndexSchema = new Schema(
+	{
 	isbn: Number,
-	created_at: {
-		type: Date,
-		default: Date.now,
-	},
+	created_at: { type: Date, default: Date.now, },
 })
 
 KoreaTime(changesIndexSchema)
 
-export default mongoose.model('ChangesIndex', changesIndexSchema)
+export default model('ChangesIndex', changesIndexSchema)
