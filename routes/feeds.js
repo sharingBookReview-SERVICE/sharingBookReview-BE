70e3c7de04e09ddb10dc7ed2c2653ec6d842e75a
SeungBin Kim <seungbin0508@gmail.com>
2021-08-23 00:04:43 +0900

    Refactor feeds to apply new (un)read reviews mechanism.

--- routes/feeds.js
+++ routes/feeds.js
@@ -1,60 +1,39 @@
 import express from 'express'
-import { Follow, Review } from '../models/index.js'
+import { Follow, Review, User } from '../models/index.js'
 import authMiddleware from '../middleware/auth_middleware.js'
 
 const router = new express.Router()
 
 router.get('/', authMiddleware(false), async (req, res, next) => {
+	/** @type {number}
+	 *  @description Number of reviews to send per request */
 	const SCROLL_SIZE = 10
 	const userId = res.locals.user?._id
-	const { lastItemId } = req.query
 
 	try {
-		let reviews
-		let result
-
-		if (!lastItemId) {
-			reviews = await Review.find()
-				.sort('-created_at')
-				.limit(SCROLL_SIZE)
-				.populate('book user')
-		} else {
-			reviews = await Review.find()
-				.sort('-created_at')
-				.where('_id')
-				.lt(lastItemId)
-				.limit(SCROLL_SIZE)
-				.populate('book user')
+		/** @type {Document}
+		 *  @description User document */
+		const user = await User.findById(userId)
+		/** @type {ObjectId[]}
+		 * @description Array of ObjectId of read reviews of user. If user is null (ie.e. not logged in) assigned as undefined. */
+		const readReviews = user?.read_reviews.map((element) => elemet.review)
+		/** @type {Object}
+		 *  @description Query statement for reviews: unread and created within one week */
+		const query = {
+			_id: { $nin: readReviews },
+			created_at: { $gte: new Date() - 1000 * 60 * 60 * 24 * 7 },
 		}
+		/** @type {Document[]}
+		 *  @description Unread reviews of user within one week */
+		const feeds = await Review.find(query).
+			sort({ created_at: -1 }).
+			limit(SCROLL_SIZE)
 
-		if (userId) {
-			result = reviews.map((review) =>
-				Review.processLikesInfo(review, userId)
-			)
-			result = await Promise.all(
-				result.map((review) =>
-					Follow.checkFollowing(review, userId, review.user)
-				)
-			)
-		} else {
-			result = reviews
-		}
-		return res.json(result)
+		return res.json({ feeds })
 	} catch (e) {
 		console.error(e)
 		return next(new Error('피드 불러오기를 실패했습니다.'))
 	}
-
-	// const userId = 'temp' //todo 로그인 안 된 상태에서 어떻게 처리할지 정해야함
-	// try {
-	// 	const reviews = await Review.find({}).populate('book user').sort('-created_at')
-	//
-	// 	const result = reviews.map(review => Review.processLikesInfo(review, userId))
-	// 	return res.json(result)
-	//
-	// } catch (err) {
-	// 	return next(new Error('피드를 불러오는데 실패했습니다.'))
-	// }
 })
 
 export default router
