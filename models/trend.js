e4f1c7e0c5c59484642369d39a96aa25e9d095b7
SeungBin Kim <seungbin0508@gmail.com>
2021-08-24 19:39:20 +0900

    Add Trends collection

---
+++ models/trend.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+const { Schema, Types, model } = mongoose
+
+// todo: 오빈님 변수명같은거 같이 고민좀 부탁드립니다..
+const trendSchema = new Schema(
+	{
+		trendPoints: [
+			{
+				review: Types.ObjectId,
+				point: Number,
+			},
+		],
+	},
+	{
+		timestamps: { createdAt: true, updatedAt: false },
+	}
+)
+
+export default model('Trend', trendSchema)
