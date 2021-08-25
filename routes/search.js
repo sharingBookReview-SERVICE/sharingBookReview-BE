f7118ca55469ae577ace459dfb7becd5de94f6f7
ohbin-kwon <ohbinkwon.dev@gmail.com>
2021-08-25 13:47:17 +0900

    make search router

--- routes/search.js
+++ routes/search.js
@@ -1,12 +1,34 @@
 import express from 'express'
 import authMiddleware from '../middleware/auth_middleware.js'
+import Collection from '../models/collection.js'
 
 const router = new express.Router()
 
-router.post('/', authMiddleware(false), async (req, res, next) => {
+router.get('/', authMiddleware(false), async (req, res, next) => {
+    try{
+        let result = await Collection.aggregate([
+            {
+            '$search': {
+                'index': 'tag', 
+                'text': {
+                    'query': `${req.query.tag}`, 
+                    'path': 'name'
+                }, 
+                'highlight': {
+                    'path': 'name'
+                }
+            }
+            }
+        ]
+        )
 
+        return res.status(201).json({result})
 
-    return res.status(201).json({})
+    }catch(e){
+        console.error(e)
+		return next(new Error('태그 검색을 실패했습니다.'))
+    }
+    
 })
 
 export default router
