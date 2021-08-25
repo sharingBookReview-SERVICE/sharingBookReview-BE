881a4f9dfa05819ca1978c9c901daa1a43e87f9e
SeungBin Kim <seungbin0508@gmail.com>
2021-08-25 18:09:30 +0900

    Revert "Update /feeds"

--- models/user.js
+++ models/user.js
@@ -1,31 +1,19 @@
 import mongoose from 'mongoose'
-const { Schema, Types, model } = mongoose
+const { Schema, model } = mongoose
 import expList from '../exp_list.js'
 
 const userSchema = new Schema({
 	nickname: String,
 	providerKey: String,
-	provider: { type: String, enum: ['naver', 'kakao', 'google'] },
-	level: { type: Number, default: 1 },
-	exp: { type: Number, default: 0 },
-	followingCount: { type: Number, default: 0 },
-	followerCount: { type: Number, default: 0 },
-	profileImage: { type: String, default: 'image_1' },
-	own_image: { type: [String], default: ['image_1'] },
+	provider: { type: String, enum: ['naver', 'kakao', 'google'], },
+	level: { type: Number, default: 1, },
+	exp: { type: Number, default: 0, },
+	followingCount: { type: Number, default: 0, },
+	followerCount: { type: Number, default: 0, },
+	profileImage: { type: String, default: 'image_1', },
+	own_image: { type: [String], default:['image_1'] },
 	// True if user received treasure. False if user can receive treasure
-	treasure: { type: Boolean, default: false },
-	read_reviews: [
-		{
-			reviewId: Types.ObjectId,
-			created_at: Date,
-		},
-	],
-	read_collections: [
-		{
-			collectionId: Types.ObjectId,
-			created_at: Date,
-		},
-	],
+	treasure: { type: Boolean, default: false, },
 })
 
 // todo 발생되는 event의 target instance id과 event를 수행하는 user id를 저장해서 level과 exp를 virtual로 표현
@@ -57,29 +45,5 @@ userSchema.statics.deleteExp = async function(userId, event) {
     await user.save()
 
 }
-
-class User {
-	refreshReadReviewsAndCollections() {
-		/**
-		 * 유저 안의 readReview s와 readCollections 를 조회해서 일주일이 지난 항목이 있으면 삭제한다.
-		 *
-		 * 본 메소드는 모두 상상속에 작성되었으며 반드시 실행테스트가 필요하다.
-		 *
-		 * 현재는 읽음 확인 기능이 없어서 테스트가 불용이하다.
-		 */
-
-		const deleteOldOnes = (arr) => {
-			return arr.filter(item => {
-				return new Date(item.created_at) < new Date(new Date() - 1000 * 60 * 60 * 24 * 7)
-			})
-		}
-
-		this.readReviews = deleteOldOnes(this.read_reviews)
-		this.readCollections = deleteOldOnes(this.read_collections)
-
-	}
-}
-
-userSchema.loadClass(User)
 
 export default model('User', userSchema)
--- routes/feeds.js
+++ routes/feeds.js
@@ -1,107 +1,60 @@
 import express from 'express'
-import { Follow, Review, User } from '../models/index.js'
+import { Follow, Review } from '../models/index.js'
 import authMiddleware from '../middleware/auth_middleware.js'
 
 const router = new express.Router()
 
 router.get('/', authMiddleware(false), async (req, res, next) => {
-	/** @type {number}
-	 *  @description Number of reviews to send per request */
 	const SCROLL_SIZE = 10
 	const userId = res.locals.user?._id
+	const { lastItemId } = req.query
 
 	try {
-		// 0. Declare constants to query.
+		let reviews
+		let result
 
-		/** @type {Document}
-		 *  @description User document */
-		const user = await User.findById(userId)
-
-		/** @type {ObjectId[]}
-		 * @description Array of ObjectId of read reviews of user. If user is null (ie.e. not logged in) assigned as undefined. */
-		const readReviews = user?.read_reviews.map((element) => element.review)
-
-		/** @type {Object}
-		 *  @description Query statement for reviews: unread and created within one week */
-		const query = {
-			_id: { $nin: readReviews },
-			created_at: { $gte: new Date() - 1000 * 60 * 60 * 24 * 7 },
+		if (!lastItemId) {
+			reviews = await Review.find()
+				.sort('-created_at')
+				.limit(SCROLL_SIZE)
+				.populate('book user')
+		} else {
+			reviews = await Review.find()
+				.sort('-created_at')
+				.where('_id')
+				.lt(lastItemId)
+				.limit(SCROLL_SIZE)
+				.populate('book user')
 		}
 
-		// 1. Return recent unread reviews of following users.
-
-		/** @type {ObjectId[]}
-		 * @description Array of user IDs that the the user in parameter is following. */
-		const followees = (await Follow.find({ sender: userId })).map(
-			(follow) => follow.receiver)
-
-		/** @type {Document[]}
-		 * @description Array of reviews of following users */
-		const followingReviews = await Review.find({
-			...query,
-			user: { $in: followees },
-		}).sort({ created_at: -1 }).limit(SCROLL_SIZE)
-
-		// If following reviews are used up (already read or no new ones) continue to next if statement
-		if (followingReviews.length) {
-			return res.json({ feeds: followingReviews })
+		if (userId) {
+			result = reviews.map((review) =>
+				Review.processLikesInfo(review, userId)
+			)
+			result = await Promise.all(
+				result.map((review) =>
+					Follow.checkFollowing(review, userId, review.user)
+				)
+			)
+		} else {
+			result = reviews
 		}
-
-		// 2. Return recent unread reviews of any users.
-
-		/** @type {Document[]}
-		 *  @description Unread reviews of user within one week */
-		const unreadReviews = await Review.find(query).
-			sort({ created_at: -1 }).
-			limit(SCROLL_SIZE)
-
-		// If unread reviews are used up (already read or no new ones) continue to last res.json()
-		if (unreadReviews.length) {
-			return res.json({ feeds: unreadReviews })
-		}
-
-		// todo: 나중에 부족하다 싶으면 뭔가 더 넣기
-
-		return res.send('finished')
+		return res.json(result)
 	} catch (e) {
 		console.error(e)
 		return next(new Error('피드 불러오기를 실패했습니다.'))
 	}
-})
 
-/**
- * Route patching read reviews
- * @name patch/:reviewId
- * @function
- * @inner
- * @param {string} path - Express path
- * @param {callback} middleware - Express middleware.
- */
-router.patch('/:reviewId', authMiddleware(true), async (req, res, next) => {
-	const { _id: userId } = res.locals.user
-	/** @type {ObjectId}
-	 * @description Review ID that user has read.
-	 */
-	const { reviewId } = req.params
-
-	/** @type {Date}
-	 * @description Created date of review.     */
-	const createdAt = (await Review.findById(reviewId)).created_at
-
-	// Find user by ID and push to read_reviews array.
-	try {
-		const user = await User.findById(userId)
-		user.read_reviews.push({
-			review: reviewId,
-			created_at: createdAt,
-		})
-		await user.save()
-
-	} catch (e) {
-		console.error(e)
-		return next(new Error('읽음 확인을 실패했습니다.'))
-	}
-
+	// const userId = 'temp' //todo 로그인 안 된 상태에서 어떻게 처리할지 정해야함
+	// try {
+	// 	const reviews = await Review.find({}).populate('book user').sort('-created_at')
+	//
+	// 	const result = reviews.map(review => Review.processLikesInfo(review, userId))
+	// 	return res.json(result)
+	//
+	// } catch (err) {
+	// 	return next(new Error('피드를 불러오는데 실패했습니다.'))
+	// }
 })
 
 export default router
