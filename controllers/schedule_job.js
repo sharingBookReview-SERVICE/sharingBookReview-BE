3cbaac6b1f541d47b6a577b6285021ad5303456c
SeungBin Kim <seungbin0508@gmail.com>
2021-08-26 00:26:50 +0900

    Update import statements

--- controllers/schedule_job.js
+++ controllers/schedule_job.js
@@ -2,26 +2,9 @@
  * Index tags of each book's reviews.
  * Operates every one hour
  */
-import { Book, ChangesIndex, Collection } from '../models/index.js'
 import schedule from 'node-schedule'
-import index_top_tags from './index_top_tags.js'
-/**
- * Returns set of isbn which have changed after the last run.
- * Marks returned documents' indexed property as true, so it can be deleted later.
- * @returns {Promise<Set<Number>>}
- */
-const getChanges = async () => {
+import indexTopTags from './index_top_tags.js'
 
-	const changes = await ChangesIndex.find()
-
-	return new Set(changes.map(change => change.isbn))
-}
-
-/**
- * 1. Execute callback every minute.
- * @type {Job}
- */
-const job = schedule.scheduleJob('30 * * * * *', async () => {
-	await index_top_tags()
+schedule.scheduleJob('0 * * * * *', async () => {
+	await indexTopTags()
 })
-
