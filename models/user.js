1e71c873feb11e2279b63b6e23d6d74eead99292
SeungBin Kim <seungbin0508@gmail.com>
2021-08-25 19:21:17 +0900

    Add read_reviews and read_collections properties in user Collection

--- models/user.js
+++ models/user.js
@@ -1,28 +1,41 @@
 import mongoose from 'mongoose'
+
 const { Schema, model } = mongoose
 import expList from '../exp_list.js'
 
 const userSchema = new Schema({
 	nickname: String,
 	providerKey: String,
-	provider: { type: String, enum: ['naver', 'kakao', 'google'], },
-	level: { type: Number, default: 1, },
-	exp: { type: Number, default: 0, },
-	followingCount: { type: Number, default: 0, },
-	followerCount: { type: Number, default: 0, },
-	profileImage: { type: String, default: 'image_1', },
-	own_image: { type: [String], default:['image_1'] },
+	provider: { type: String, enum: ['naver', 'kakao', 'google'] },
+	level: { type: Number, default: 1 },
+	exp: { type: Number, default: 0 },
+	followingCount: { type: Number, default: 0 },
+	followerCount: { type: Number, default: 0 },
+	profileImage: { type: String, default: 'image_1' },
+	own_image: { type: [String], default: ['image_1'] },
 	// True if user received treasure. False if user can receive treasure
-	treasure: { type: Boolean, default: false, },
+	treasure: { type: Boolean, default: false },
+	read_reviews: [
+		{
+			_id: Types.ObjectId,
+			created_at: Date,
+		},
+	],
+	read_collections: [
+		{
+			_id: Types.ObjectId,
+			created_at: Date,
+		},
+	],
 })
 
 // todo 발생되는 event의 target instance id과 event를 수행하는 user id를 저장해서 level과 exp를 virtual로 표현
 // why 누가 어디에 event를 실행했는지를 저장하는것이 확장성이 높다.
-userSchema.statics.getExpAndLevelUp = async function(userId, event) {
-    const user = await this.findById(userId)
-    // event에 따른 경험치를 획득
-    user.exp += expList[event]
-    // Required experience to next level.
+userSchema.statics.getExpAndLevelUp = async function (userId, event) {
+	const user = await this.findById(userId)
+	// event에 따른 경험치를 획득
+	user.exp += expList[event]
+	// Required experience to next level.
 	const requiredExp = 3 * 1.05 ** (user.level - 1)
 
 	// Level up
@@ -37,13 +50,12 @@ userSchema.statics.getExpAndLevelUp = async function(userId, event) {
 	return user.treasure
 }
 
-userSchema.statics.deleteExp = async function(userId, event) {
-    const user = await this.findById(userId)
+userSchema.statics.deleteExp = async function (userId, event) {
+	const user = await this.findById(userId)
 
-    user.exp -= expList[event]
-    
-    await user.save()
+	user.exp -= expList[event]
 
+	await user.save()
 }
 
 export default model('User', userSchema)
