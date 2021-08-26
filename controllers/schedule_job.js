2b26a97b38a78c1a80b6b90675c6e50d2c2437b1
SeungBin Kim <seungbin0508@gmail.com>
2021-08-26 19:25:34 +0900

    Update scheduleJob to run every day at 20:00

--- controllers/schedule_job.js
+++ controllers/schedule_job.js
@@ -4,7 +4,11 @@
  */
 import schedule from 'node-schedule'
 import indexTopTags from './index_top_tags.js'
+import getTrendingReviews from './get_trending_reviews.js'
 
-schedule.scheduleJob('0 * * * * *', async () => {
+// per minute '0 * * * * *'
+schedule.scheduleJob('20 * * *', async () => {
+	console.log('스케줄 시작')
 	await indexTopTags()
+	await getTrendingReviews()
 })
