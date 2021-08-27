dd1c4cc15281d28194fde67f76627cdc0636fd99
ohbin-kwon <ohbinkwon.dev@gmail.com>
2021-08-27 12:40:28 +0900

    book mark put route
    toggle of bookmark route

--- routes/reviews.js
+++ routes/reviews.js
@@ -152,4 +152,30 @@ router.delete('/:reviewId', async (req, res) => {
 
 router.put('/:reviewId/likes', await likeUnlike(Review, 'review'))
 
+router.put('/:reviewId/bookmark', authMiddleware(true), async (req, res, next) => {
+    const userId = res.locals.user._id
+	const { reviewId } = req.params
+    let status
+
+	try {
+        let user = await User.findById(userId)
+		const { bookmark_reviews } = user
+
+        if(bookmark_reviews.includes(reviewId)){
+            bookmark_reviews.pull(reviewId)
+            status = false
+
+        }else{
+            bookmark_reviews.push(reviewId)
+            status = true
+        }
+        
+        await user.save()
+
+		return res.status(202).json({status})
+	} catch (e) {
+		return next(new Error('북마크 등록을 실패했습니다.'))
+	}
+})
+
 export default router
