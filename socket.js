fbc56ab9d70c51746a21dc5a1126c66446ee8788
ohbin-kwon <ohbinkwon.dev@gmail.com>
2021-08-28 12:28:34 +0900

    get token channel

--- socket.js
+++ socket.js
@@ -5,17 +5,11 @@ const connectedUser = {}
 //todo: 소켓 연결 자체를 로그인한 사람들만 연결해야함/ 즉 로그인 안한사람들은 연결할 필요가 없음
 io.on('connection', (socket) => {
     // front에서 extraHeaders에 값을 담아보내면 handshake.headers에 값을 받을 수 있다.
-    const { token } = socket.handshake.headers
-    const userId = validateToken({token})
-    connectedUser[userId] = socket.id
-    console.log(connectedUser)
-
-    // socket.emit('sendToken')
-    // socket.on('receiveToken', (token) => {
-    //     const userId = validateToken({token})
-    //     connectedUser[userId] = socket.id
-    //     console.log(connectedUser)
-    // } )
+    socket.on('token', (token) => {
+        const userId = validateToken({token})
+        connectedUser[userId] = socket.id
+        console.log(connectedUser)
+    })
     
     socket.on('disconnect', () =>{
         console.log('연결해제')
