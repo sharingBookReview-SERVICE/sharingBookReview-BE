6dfecad92c24bddb8fcf868903aebcd998da2b25
SeungBin Kim <seungbin0508@gmail.com>
2021-09-27 19:59:16 +0900

    update schedule.controller.js to import tagController instead of indexTopTags function

--- controllers/schedule.controller.js
+++ controllers/schedule.controller.js
@@ -1,9 +1,9 @@
-import indexTopTags from './tag.controller.js'
 import getTrendingReviews from './get_trending_reviews.js'
+import tagController from './tag.controller.js'
 
 const DAY = 1000 * 60 * 60 * 24
 setInterval(async () => {
 	console.log('스케쥴 시작')
-	await indexTopTags()
+	await tagController.updateTopTags()
 	await getTrendingReviews()
 }, DAY)
