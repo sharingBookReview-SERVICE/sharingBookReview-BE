751e1cabb3085ece42474c1e6aaf4ca90154a193
SeungBin Kim <seungbin0508@gmail.com>
2021-09-28 01:36:10 +0900

    Update removeArrayInValue to reformatSearchResult
    1. Changed name of method to increase readability
    
    2. Used Array.prototype.map instead of for loop

--- controllers/crawl.controller.js
+++ controllers/crawl.controller.js
@@ -25,58 +25,15 @@ export default class crawlController {
 		const searchResult = parsedString.rss.channel[0].item
 		return crawlController.reformatSearchResult(searchResult)
 	}
-}
 
-const removeArrayInValue = async (array) => {
-	const bookList =[]
-	for (let i = 0; i < array.length; i++){
-		const book = {}
-		for (const [key,value] of Object.entries(array[i])){
-			if(key == 'isbn'){
-				book[key] = value[0].split(' ')[1]
-			}else{
-				book[key] = value[0]
+	static reformatSearchResult(searchResult) {
+		return searchResult.map(book => {
+			const object = {}
+			for (const [key, value] of Object.entries(book)) {
+				if (key === 'isbn') object[key] = value[0].split(' ')[1]
+				else object[key] = value[0]
 			}
-		}
-		bookList.push(book)
+			return object
+		})
 	}
-	return bookList
-}
-
-/**
- * Search book information on naver.
- * @param {string} target - Search target must be either d_titl, d_auth, or d_isbn
- * @param {string} query - Search query
- * @returns {Promise<Object[]>}
- */
-const searchBooks = async (target, query) => {
-	const client_id = process.env.BOOK_API_CLIENT_ID
-	const client_secret = process.env.BOOK_API_CLIENT_SECRET
-	const targetConverter = {
-		제목: 'd_titl',
-		저자: 'd_auth',
-		출판사: 'd_publ',
-		isbn: 'd_isbn',
-	}
-	const api_url =
-		`https://openapi.naver.com/v1/search/book_adv.xml?${targetConverter[target]}=` +
-		encodeURI(query)
-	const result = await axios({
-		method: 'get',
-		url: api_url,
-
-		headers: {
-			'X-Naver-Client-Id': client_id,
-			'X-Naver-Client-Secret': client_secret,
-		},
-	}) // todo 프로미스로 이어 보자
-	let searchList
-	parseString(result.data, (err, result, next) => {
-		if (err) {
-			console.error(err)
-			return next(err)
-		}
-		searchList = result.rss.channel[0].item
-	})
-	return removeArrayInValue(searchList)
 }
