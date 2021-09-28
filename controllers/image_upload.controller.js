79dc67242499436fdfe54cd3f6ceb7d7ca6f4536
SeungBin Kim <seungbin0508@gmail.com>
2021-09-28 17:09:58 +0900

    Add image_upload.controller.js and Move image_upload.js into it

---
+++ controllers/image_upload.controller.js
@@ -0,0 +1,20 @@
+import { uploadFile } from '../s3.js'
+import util from 'util'
+import fs from 'fs'
+
+const unlinkFile = util.promisify(fs.unlink)
+
+export default class ImageUploadController {
+
+	static async uploadImage(req, res, next) {
+		const file = req.file
+		if (file === undefined) {
+			return next()
+		}
+		const result = await uploadFile(file)
+		await unlinkFile(file.path)
+		res.locals.url = result.Location
+		return next()
+
+	}
+}
